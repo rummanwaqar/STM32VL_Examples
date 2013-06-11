68d5750dc26c60270636f6d852dba5e0251429d2
Rumman Waqar <rumman.waqar05@gmail.com>
2013-06-11 02:06:34 -0600

    Added GPIO Example

--- GPIO/src/main.c
+++ GPIO/src/main.c
@@ -2,27 +2,34 @@
 #include <stm32f10x_rcc.h>
 #include <stm32f10x_gpio.h>
 
-void Delay(uint32_t nTimes);
+void Delay(uint32_t nTime);
 
 int main(void) {
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOA, ENABLE);
+  
   GPIO_InitTypeDef GPIO_InitStructure;
-
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-
   GPIO_StructInit(&GPIO_InitStructure);
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_8 | GPIO_Pin_9;
   GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
   GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-  if (SysTick_Config(SystemCoreClock / 1000))
-    while (1);
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
 
+  if (SysTick_Config(SystemCoreClock / 1000))
+    while(1);
+
+  static int ledVal = 0;
   while(1) {
-    static int ledVal = 0;
-    GPIO_WriteBit(GPIOC, GPIO_Pin_9, (ledVal) ? Bit_SET : Bit_RESET);
-    ledVal = 1 - ledVal;
-    Delay(250);
+    if(GPIO_ReadInputDataBit(GPIOA, GPIO_Pin_0)) {
+      GPIO_WriteBit(GPIOC, GPIO_Pin_8, (ledVal) ? Bit_SET : Bit_RESET);
+      GPIO_WriteBit(GPIOC, GPIO_Pin_9, (ledVal) ? Bit_SET : Bit_RESET);
+      ledVal = 1 - ledVal;
+    }
+    Delay(150);
   }
 }
 
@@ -30,18 +37,17 @@ static __IO uint32_t TimingDelay;
 
 void Delay(uint32_t nTime) {
   TimingDelay = nTime;
-  while(TimingDelay != 0);
+  while (TimingDelay != 0);
 }
 
 void SysTick_Handler(void) {
-  if (TimingDelay != 0x00) {
+  if(TimingDelay != 0x00) {
     TimingDelay--;
   }
 }
 
 #ifdef USE_FULL_ASSERT
 void assert_failed(uint8_t *file, uint32_t line) {
-  /* Infinite loop */
   while(1);
 }
 #endif
